8aef1738883208c31c11b893fbf4d128f04536a5
minjumost <95904729+minjumost@users.noreply.github.com>
2023-06-16 02:20:18 +0900

    재고관리 mvt 1차 작성완료 내일 마무리 지을 예정

--- manager/urls.py
+++ manager/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('menu/', include('beverage.urls')),
+    path('stock/', include('ingredient.urls')),
 ]
