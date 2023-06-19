13b4ae11bb6a39fab521a93cbd6f90e444986268
minjumost <minjumost@naver.com>
2023-06-19 16:22:42 +0900

    Ingredient CRUD
    재료 모델에 대한 CRUD 완성

--- ingredient/urls.py
+++ ingredient/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('stock/', views.ingredient),
     path('', views.home),
+    path('stock/', views.ViewIngredients.as_view()),
+    path('stock/create/', views.CreateIngredient.as_view()),
+    path('stock/update/<int:pk>/', views.UpdateIngredient.as_view()),
+    path('stock/delete/<int:pk>/', views.DeleteIngredient.as_view()),
 ]
--- ingredient/views.py
+++ ingredient/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 from .models import Ingredient
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 # Create your views here.
-def ingredient(request):
-    ingredients = Ingredient.objects.all()
-    return render(
-        request,
-        'ingredient/ingredient.html',
-        {
-            'ingredients': ingredients,
-        }
-    )
 
 def home(request):
     return render(
         request,
         'ingredient/home.html',
     )
+
+class ViewIngredients(ListView):
+    model = Ingredient
+    template_name = 'ingredient/list_ingredient.html'
+class CreateIngredient(CreateView):
+    model = Ingredient
+    fields = ['name', 'ammount', 'exp_date', 'min_ammount']
+    template_name = 'ingredient/create_ingredient.html'
+    success_url = '/stock/'
+
+class UpdateIngredient(UpdateView):
+    model = Ingredient
+    fields = ['name', 'ammount', 'exp_date', 'min_ammount']
+    success_url = '/stock/'
+    template_name = 'ingredient/update_ingredient.html'
+
+class DeleteIngredient(DeleteView):
+    model = Ingredient
+    template_name = 'ingredient/delete_ingredient.html'
+    success_url = '/stock/'
